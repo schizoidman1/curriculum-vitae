4f4ca18d91bc29a60ccdc835892c5ed3a5cd4fcc
Zurita027 <139669047+Zurita027@users.noreply.github.com>
2024-04-25 10:35:29 -0300

    main

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const skillsSection = document.querySelectorAll('.skills');
     const workSection = document.querySelectorAll('.work');
     const portfolioSection = document.querySelectorAll('.portfolio');
+    const contactSection = document.querySelectorAll('.contact')
 
     anchorLinks.forEach(link => {
         link.addEventListener('click', function(event) {
@@ -22,6 +23,9 @@ document.addEventListener("DOMContentLoaded", function() {
             portfolioSection.forEach(section => {
                 section.style.display = 'none';
             });
+            contactSection.forEach(section => {
+                section.style.display = 'none';
+            });
 
             const targetSectionClass = this.getAttribute('href').substring(1); // Get the class of the target section
 
@@ -38,6 +42,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 portfolioSection.forEach(section => {
                     section.style.display = 'block';
                 });
+            } else if (targetSectionClass === 'contact') {
+                contactSection.forEach(section => {
+                    section.style.display = 'block';
+                });
             } else {
                 // Default to main section if no specific class matches
                 mainSection.forEach(section => {
@@ -47,3 +55,15 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+function sendEmail() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    const subject = `Message from ${name}`;
+    const body = `Name: ${name}\nEmail: ${email}\n\n${message}`;
+
+    window.location.href = `mailto:luccaquintas@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+}
